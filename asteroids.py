aa8b6838f8f91c313a2cc5d234edfdbef38059d4
Marcus Caisey <34950778+marcuscaisey@users.noreply.github.com>
2018-02-08 03:59:38 +0000

    WIP

--- asteroids.py
+++ asteroids.py
@@ -1,5 +1,6 @@
 from __future__ import division
 import pygame
+import pygame.freetype
 from pygame.math import Vector2
 from pylygon import Polygon
 from math import sin, cos, radians
@@ -320,17 +321,25 @@ class AsteroidsGame:
 
     INITIAL_ASTEROIDS = 4  # Number of asteroids at start of game
     START_OF_ROUND_TIME = 3  # Seconds to wait before starting new round
+    # Scores for destroying each asteroid size
+    SCORE = {
+        3: 20,
+        2: 50,
+        1: 100,
+    }
     MAX_FPS = 60
 
     def __init__(self, screen_width, screen_height):
         w, h = screen_width, screen_height
         self.surface = pygame.display.set_mode((w, h), pygame.FULLSCREEN)
-        self.screen_area = Polygon([(0, 0), (w, 0), (w, h), (0, h)])
         pygame.display.set_caption('Asteroids')
+        self.screen_area = Polygon([(0, 0), (w, 0), (w, h), (0, h)])
         pygame.mouse.set_visible(False)
         self.clock = pygame.time.Clock()
-        self.exit = False
         self.initialise_constants(screen_width, screen_height)
+        self.exit = False
+        self.score_font = pygame.freetype.Font('Hyperspace.otf', h // 15)
+        self.highscore = 0
         self.reset()
 
     def initialise_constants(self, screen_width, screen_height):
@@ -346,6 +355,7 @@ class AsteroidsGame:
         """Start a new game."""
         self.playing = True
         self.round = 0
+        self.score = 0
         self.start_of_round_timer = 0
         self.ship = Ship()
         self.asteroids = []
@@ -361,6 +371,19 @@ class AsteroidsGame:
         """Return list of 3 + self.round asteroids."""
         number_of_asteroids = AsteroidsGame.INITIAL_ASTEROIDS + self.round - 1
         return [Asteroid() for _ in range(number_of_asteroids)]
+
+    def destroy_asteroid(self, asteroid):
+        """
+        Remove asteroid from asteroids list and add two more smaller
+        asteroids if asteroid is not smallest size.
+        """
+        self.asteroids.remove(asteroid)
+        if asteroid.size > 1:
+            self.asteroids.extend(asteroid.split())
+
+    # draw_text function instead!!!
+    def draw_score(self):
+        self.score_font.render_to(self.surface, (5, 5), str(self.score), WHITE)
 
     def event_handler(self):
         # could this be made more readable?
@@ -386,15 +409,6 @@ class AsteroidsGame:
                 elif event.key == pygame.K_UP:
                     self.ship.boosting = False
 
-    def destroy_asteroid(self, asteroid):
-        """
-        Remove asteroid from asteroids list and add two more smaller
-        asteroids if asteroid is not smallest size.
-        """
-        self.asteroids.remove(asteroid)
-        if asteroid.size > 1:
-            self.asteroids.extend(asteroid.split())
-
     def update(self, dt):
         """Update game by dt seconds."""
         self.ship.update(self.screen_area, dt)
@@ -405,6 +419,7 @@ class AsteroidsGame:
                 if self.ship.hits(asteroid):
                     self.destroy_asteroid(asteroid)
                 elif self.ship.shoots(asteroid):
+                    self.score += AsteroidsGame.SCORE[asteroid.size]
                     self.destroy_asteroid(asteroid)
         else:
             self.start_of_round_timer += dt
@@ -418,6 +433,7 @@ class AsteroidsGame:
         self.ship.draw(self.surface)
         for asteroid in self.asteroids:
             asteroid.draw(self.surface)
+        self.draw_score()
 
     def draw_end_screen(self):
         """Draw game after game over."""
