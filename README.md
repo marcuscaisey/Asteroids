a24736efd8cdda032e84d2bff937207532479d9f
Marcus Caisey <34950778+marcuscaisey@users.noreply.github.com>
2018-02-14 03:05:50 +0000

    Add end screen and update README

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Asteroids
-Asteroids game written in Python using pygame
+
+Asteroids game written in Python using pygame. Game is written in Python 2 since that's what polygon module (pylygon) used for collision detection of polygons is written in.
+
+### Prerequisites
+
+`pygame`
+`pylgon`
+`numpy`
--- hud.py
+++ hud.py
@@ -1,7 +1,7 @@
 import pygame.freetype
 
-from ship import Ship
 from screen_constants import HEIGHT
+from ship import Ship
 
 WHITE = (255, 255, 255)
 
@@ -15,12 +15,16 @@ class HUD:
     def __init__(self):
         pygame.freetype.init()
         self.font = pygame.freetype.Font('Hyperspace.otf', SCORE_SIZE)
+        # life icon is just a ship object which only gets drawn
         self.icon = Ship(ICON_LENGTH, self.icon_centre())
 
     def icon_centre(self):
-        text_height = self.font.get_rect('0', size=SCORE_SIZE).height
+        """Return centre for life icon."""
+        # This aligns icon horizontally with score
         x = 0.36 * ICON_LENGTH
-        y = 0.67 * ICON_LENGTH + text_height + 0.015 * HEIGHT
+        # This moves icon below score
+        score_height = self.font.get_rect('0', size=SCORE_SIZE).height
+        y = 0.67 * ICON_LENGTH + score_height + 0.015 * HEIGHT
         return (x, y)
 
     def draw(self, surface, score, lives):
